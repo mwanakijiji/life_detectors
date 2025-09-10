fixed units bug in zodiacal calculation

modules/core/calculator.py
@@ -11,6 +11,7 @@ from dataclasses import dataclass
 import logging
 import configparser
 import ipdb
+import astropy.units as u
 import matplotlib.pyplot as plt
 
 from .astrophysical import AstrophysicalSources
@@ -61,6 +62,7 @@ class NoiseCalculator:
 
         # the object that is the 'origin' of the various noise contributions for the calculations to follow
         self.noise_origin = noise_origin
+        #self.prop_dict = noise_origin.prop_dict
 
         #self.unit_converter = UnitConverter()
         #self.conversion_engine = ConversionEngine(self.unit_converter)
@@ -84,7 +86,9 @@ class NoiseCalculator:
 
         ## everything in units of photoelectrons
 
-        wavel_abcissa = self.noise_origin.prop_dict['wavel']
+        ## ## TO DO: MAKE ALL WAVELS TO BE ON AN EXPLICIT COMMON BASIS
+        #wavel_abcissa = self.noise_origin.prop_dict['wavel']
+        wavel_abcissa = self.noise_origin.sources_astroph['star']['wavel']
         # map wavelengths to pixels
         res_spec = float(self.config["detector"]["spec_res"]) # spectral resolution (del_lambda/lambda)
         del_lambda_array = wavel_abcissa / res_spec # size of wavelength bins (um)
@@ -96,12 +100,28 @@ class NoiseCalculator:
         #n_pix = 1 / disp # pixels per micron(pix/um)
         #pix_abcissa = n_pix*wavel_abcissa - np.min(n_pix*wavel_abcissa) # remove offset
 
+        
+
         # integration time for 1 frame
-        t_int = float(self.config["observation"]["integration_time"])
+        t_int = float(self.config["observation"]["integration_time"]) * u.second
         n_int = float(self.config["observation"]["n_int"])
 
         # total science (planet) signal (note this is not a function of time)
         # _prime denotes it is not measured directly (i.e., photoelectrons and not ADU)
+        ipdb.set_trace()
+
+        # FYI plot: rate of photoelectrons to be expected from sources
+        plt.clf()
+        plt.plot(self.noise_origin.prop_dict['exoplanet']['wavel'],self.noise_origin.prop_dict['exoplanet']['flux_post_aperture_ph_sec_um'], label='exoplanet')
+        plt.plot(self.noise_origin.prop_dict['star']['wavel'],self.noise_origin.prop_dict['star']['flux_post_aperture_ph_sec_um'], label='star')
+        plt.plot(self.noise_origin.prop_dict['zodiacal']['wavel'],self.noise_origin.prop_dict['zodiacal']['flux_post_aperture_ph_sec_um'], label='zodiacal')
+        #plt.plot(self.noise_origin.prop_dict['exozodiacal']['wavel'],self.noise_origin.prop_dict['exozodiacal']['flux_post_aperture_ph_sec_um'], label='exozodiacal')
+        plt.legend()
+        plt.xlabel('Wavelength (um)')
+        plt.ylabel('Rate of photoelectrons (e-/sec/um)')
+        plt.title('Rate of photoelectrons to be expected from sources')
+        plt.savefig('/Users/eckhartspalding/Downloads/rate_of_photoelectrons_from_sources.png')
+
         del_Np_prime_del_t = self.noise_origin.prop_dict['exoplanet_flux_e_sec_um'] * del_lambda_array # approximates an integral over lambda (final units are e/sec/um * um = e/sec)
         # stellar signal
         del_Ns_prime_del_t = self.noise_origin.prop_dict['star_flux_e_sec_um'] * del_lambda_array # approximates an integral over lambda 

modules/utils/helpers.py
@@ -254,7 +254,7 @@ def generate_zodiacal_spectrum(config: configparser.ConfigParser, wavelength_um:
         indexing='ij'
     )
     # for 2D FYI plot
-    wavelengths_to_plot_2d = [5, 10, 20]  # microns (unitless; this is just for plotting)
+    wavelengths_to_plot_2d = [5, 10, 20] * u.um  # microns (unitless; this is just for plotting)
     I_lambda_2d_energy = {} # dict to contain the 2D arrays showing emission at each wavelength
     I_nu_2d_energy = {} # dict to contain the 2D arrays showing emission at each wavelength
     I_lambda_2d_photons = {} # dict to contain the 2D arrays showing emission at each wavelength
@@ -264,11 +264,11 @@ def generate_zodiacal_spectrum(config: configparser.ConfigParser, wavelength_um:
 
         # units W / (um * sr * m2)
         term_i_2d = bb_1(wavel_this) + A_albedo * bb_2(wavel_this) * ( rad_sol / 1.5 ) ** 2
-        # unitless
-        term_ii_2d = np.sqrt( ( np.pi/np.arccos(np.cos(lambda_rel_lon_grid * np.pi/180.) * np.cos(beta_lat_grid * np.pi/180.)) ) / ( (np.sin(beta_lat_grid * np.pi/180.) ** 2.) + 0.36 * (wavel_this / 11.)**(-0.8) * np.cos(beta_lat_grid * np.pi/180.) ** 2.) )
+        # unitless; note the wavel_this/u.um is necessary to avoid math errors
+        term_ii_2d = np.sqrt( ( np.pi/np.arccos(np.cos(lambda_rel_lon_grid * np.pi/180.) * np.cos(beta_lat_grid * np.pi/180.)) ) / ( (np.sin(beta_lat_grid * np.pi/180.) ** 2.) + 0.36 * ((wavel_this/u.um) / 11.)**(-0.8) * np.cos(beta_lat_grid * np.pi/180.) ** 2.) )
         
         I_lambda_2d_energy[str(wavel_this)] = tau_opt * term_i_2d * term_ii_2d # for units W  / (micron sr m2)
-        I_nu_2d_energy[str(wavel_this)] = (I_lambda_2d_energy[str(wavel_this)] * (wavel_this*u.um)**2 / const.c).to(u.MJy / u.sr) # for units MJy/sr; note the plotted wavel_this is unitless, so have to tack on units here
+        I_nu_2d_energy[str(wavel_this)] = (I_lambda_2d_energy[str(wavel_this)] * (wavel_this)**2 / const.c).to(u.MJy / u.sr) # for units MJy/sr; note the plotted wavel_this is unitless, so have to tack on units here
         #I_lambda_2d_photons[str(wavel_this)] = I_lambda_2d_energy[str(wavel_this)] * u.photon / (const.h * const.c / wavel_this).to # for units 1 / (micron s)
     
     # make a full spectrum of the emission along the line-of-sight
@@ -299,7 +299,7 @@ def generate_zodiacal_spectrum(config: configparser.ConfigParser, wavelength_um:
         fig, axes = plt.subplots(1, 3, figsize=(15, 4))
         for i, wl in enumerate(wavelengths_to_plot_2d):
             # Find the index in wavelength_um closest to wl
-            idx = np.abs(wavelength_um/u.um - wl).argmin()
+            idx = np.abs(wavelength_um/u.um - wl/u.um).argmin()
             im = axes[i].imshow(I_nu_2d_energy[str(wl)].value, origin='lower', 
                                extent=[np.min(lambda_rel_lon_grid), np.max(lambda_rel_lon_grid), np.min(beta_lat_grid), np.max(beta_lat_grid)], aspect='auto')
             axes[i].set_title(f'Zodiacal background\nat {wavelength_um[idx]/u.um:.1f} μm')